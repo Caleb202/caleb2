2bb7051664a6b9ed03719ccd28925f0ca19833f7
Caleb202 <67674472+Caleb202@users.noreply.github.com>
2020-06-30 23:04:07 -0600

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,61 @@
+
+//Numerico
+
+var edad = 23.222;
+
+//string
+var nombre = Caleb;
+var apellido = Cortes;
+
+document.write(nombre + " " apellido);
+
+var texto = 'Caleb Cortes es "guapo"'
+
+//Array- Arreglo
+
+var amigos = ["Caleb", "Josue"];
+document.write(amigos[0]);
+
+//objetos
+
+var objecto ={
+	color"black",
+	size: 12;
+}
+
+//boleano
+
+var boleano = true;
+
+//indefinido
+
+var caleb;
+
+document.write()
+
+//null
+
+var numero2 = 10;
+var numero = null;
+
+//NaN
+
+var texto2 = "hola como estas"
+document.write(texto*2)
+
+
+
+//var nombre;
+
+//nombre = "Caleb"
+
+//edad = 16 + 1;
+
+//document.write(nombre+" " + edad);
+
+//alert(nombre);
+//esto es una instruccion de practica;
+
+
+
+
